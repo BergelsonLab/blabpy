664fb6ee360d7d8f64e2ab1128f9d63e7fdccfad
Zhenya <e.kalenkovich@gmail.com>
2022-01-15 15:21:02 -0500

    test that all cha structure files are present

--- blabpy/seedlings/tests/test_on_actual_data.py
+++ blabpy/seedlings/tests/test_on_actual_data.py
@@ -1,6 +1,12 @@
 import pytest
+from pathlib import Path
 
 import pandas as pd
+
+from blabpy.seedlings.paths import ALL_CHILDREN, ALL_MONTHS, MISSING_AUDIO_RECORDINGS
+
+
+CHA_STRUCTURE_FILENAME_PATTERN = '{child:02}_{month:02}_sparse_code.cha.txt'
 
 
 @pytest.fixture(scope='module')
@@ -8,5 +14,10 @@ def total_listen_time_summary_df(total_listen_time_summary_csv):
     return pd.read_csv(total_listen_time_summary_csv)
 
 
-def test_the_whole_thing(total_listen_time_summary_df, cha_structures_folder):
-    assert False
+@pytest.mark.parametrize(argnames=('child', 'month'),
+                         argvalues=[(child, month)
+                                    for child in ALL_CHILDREN for month in ALL_MONTHS
+                                    if (child, month) not in MISSING_AUDIO_RECORDINGS])
+def test_the_whole_thing(total_listen_time_summary_df, cha_structures_folder, child, month):
+    cha_structure_path = Path(cha_structures_folder) / CHA_STRUCTURE_FILENAME_PATTERN.format(child=child, month=month)
+    assert cha_structure_path.exists()
