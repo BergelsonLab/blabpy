e3fc8750791c5114b76a7db6bee39ce548d67b13
Zhenya <e.kalenkovich@gmail.com>
2022-02-25 11:11:38 -0500

    fix(tests): update listen time stats test values
    - the last version of blabpy/seedlings/tests/data/listen_time_stats.csv was done on an outdated copy of the cha files,
    - switch test to use data from GitHub not PN-OPUS
    
    Note: the behavior has not changed - not bumping the version

--- blabpy/seedlings/tests/test_on_actual_data.py
+++ blabpy/seedlings/tests/test_on_actual_data.py
@@ -7,6 +7,7 @@ import pandas as pd
 from blabpy.seedlings.cha import export_cha_to_csv
 from blabpy.seedlings.listened_time import listen_time_stats_for_report, _get_subregion_count
 from blabpy.seedlings.paths import ALL_CHILDREN, ALL_MONTHS, MISSING_AUDIO_RECORDINGS, get_cha_path
+from blabpy.utils import text_file_checksum
 
 
 def _possibly_interpret_as_list(possibly_list):
@@ -14,6 +15,10 @@ def _possibly_interpret_as_list(possibly_list):
         return ast.literal_eval(possibly_list)
     else:
         return possibly_list
+
+
+def _get_test_cha_path(child, month):
+    return Path(f'data/annotated_cha/annotated_cha/{child:02}_{month:02}_sparse_code.cha')
 
 
 @pytest.fixture(scope='module')
@@ -25,9 +30,9 @@ def listen_time_stats_df():
                          argvalues=[(child, month)
                                     for child in ALL_CHILDREN for month in ALL_MONTHS
                                     if (child, month) not in MISSING_AUDIO_RECORDINGS])
-def test_the_whole_thing(listen_time_stats_df, child, month):
+def test_listen_time_stats_for_report(listen_time_stats_df, child, month):
     subregion_count = _get_subregion_count(child=child, month=month)
-    cha_path = get_cha_path(child=child, month=month)
+    cha_path = _get_test_cha_path(child, month)
     stats_correct = listen_time_stats_df.set_index('filename').loc[cha_path.name].to_dict()
 
     stats = listen_time_stats_for_report(clan_file_text=cha_path.read_text(), subregion_count=subregion_count)
@@ -47,7 +52,7 @@ def cha_export_checksums_df():
                                     for child in ALL_CHILDREN for month in ALL_MONTHS
                                     if (child, month) not in MISSING_AUDIO_RECORDINGS])
 def test_export_cha_to_csv(cha_export_checksums_df, child, month, tmpdir):
-    cha_path = Path(f'data/annotated_cha/annotated_cha/{child:02}_{month:02}_sparse_code.cha')
+    cha_path = _get_test_cha_path(child, month)
     cha_checksum = text_file_checksum(cha_path)
     export_cha_to_csv(cha_path, tmpdir)
     exported_csv_path = tmpdir / cha_path.name.replace(".cha", "_processed.csv")
