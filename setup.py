d468da9b67b5be34abec0990065a368d03365a19
Zhenya <e.kalenkovich@gmail.com>
2022-03-07 16:35:19 -0500

    bump version to 0.5.0
    When creating random segments for annotations of VIHI recordings, stop if some of the output files already exist to avoid overwriting them.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="blabpy",
-    version="0.4.0",
+    version="0.5.0",
     packages=find_packages(),
     python_requires='>=3.7',
     install_requires=['pandas', 'numpy', 'pyarrow', 'pympi-ling'],
