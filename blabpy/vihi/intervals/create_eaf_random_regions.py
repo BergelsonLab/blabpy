f30e7d044ee245fff93a210ee0e1f913758c77db
Zhenya <e.kalenkovich@gmail.com>
2022-04-20 17:28:50 -0400

    fix: no more output_dir for the cli

--- blabpy/vihi/intervals/create_eaf_random_regions.py
+++ blabpy/vihi/intervals/create_eaf_random_regions.py
@@ -6,6 +6,5 @@ from blabpy.vihi.intervals.intervals import batch_create_files_with_random_regio
 
 if __name__ == "__main__":
     info_spreadsheet_path = Path(sys.argv[1])
-    output_dir = sys.argv[2]
-    seed = None if len(sys.argv) <= 3 else int(sys.argv[3])
-    batch_create_files_with_random_regions(info_spreadsheet_path, output_dir, seed=seed)
+    seed = None if len(sys.argv) <= 2 else int(sys.argv[2])
+    batch_create_files_with_random_regions(info_spreadsheet_path, seed=seed)
