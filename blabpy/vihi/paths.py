f6776c7b1e0b4e39114a6a1cb7349952da8a9095
Zhenya <e.kalenkovich@gmail.com>
2022-08-29 20:57:26 -0400

    feat: add functions to find eaf, its, and rttm files
    We will need them soon to get existing annotation intervals, get information about when the recording was on, and get VTC-produced data.

--- blabpy/vihi/paths.py
+++ blabpy/vihi/paths.py
@@ -121,3 +121,66 @@ def get_lena_recording_path(population, subject_id, recording_id):
     _check_id_string(subject_id)
     _check_id_string(recording_id)
     return get_lena_subject_path(population, subject_id) / compose_full_recording_id(population, subject_id, recording_id)
+
+
+def get_its_path(population, subject_id, recording_id):
+    """
+    Find the .its file.
+    :param population: one of POPULATIONS
+    :param subject_id: zero-padded three-digit string
+    :param recording_id: --//--
+    :return: Path object
+    """
+    _check_population(population)
+    _check_id_string(subject_id)
+    _check_id_string(recording_id)
+
+    subject_dir = get_lena_subject_path(population, subject_id)
+    assert subject_dir.exist(), 'Can\'t find the subject folder. Check the arguments.'
+
+    full_recording_id = compose_full_recording_id(population, subject_id, recording_id)
+    its_path = subject_dir / f'{full_recording_id}.its'
+
+    return its_path
+
+
+def get_rttm_path(population, subject_id, recording_id):
+    """
+    Find the .rttm file with the VTC output for a single recording.
+    :param population: one of POPULATIONS
+    :param subject_id: zero-padded three-digit string
+    :param recording_id: --//--
+    :return: Path object
+    """
+    _check_population(population)
+    _check_id_string(subject_id)
+    _check_id_string(recording_id)
+
+    subject_dir = get_lena_subject_path(population, subject_id)
+    assert subject_dir.exist(), 'Can\'t find the subject folder. Check the arguments.'
+
+    full_recording_id = compose_full_recording_id(population, subject_id, recording_id)
+    rttm_path = subject_dir / 'derivatives' / 'vtc' / 'separated' / full_recording_id / 'all.rttm'
+
+    return rttm_path
+
+
+def get_eaf_path(population, subject_id, recording_id):
+    """
+    Find the .eaf file with the ACLEW-style annotations for a single recording.
+    :param population: one of POPULATIONS
+    :param subject_id: zero-padded three-digit string
+    :param recording_id: --//--
+    :return: Path object
+    """
+    _check_population(population)
+    _check_id_string(subject_id)
+    _check_id_string(recording_id)
+
+    subject_dir = get_lena_subject_path(population, subject_id)
+    assert subject_dir.exist(), 'Can\'t find the subject folder. Check the arguments.'
+
+    full_recording_id = compose_full_recording_id(population, subject_id, recording_id)
+    eaf_path = subject_dir / f'{full_recording_id}.eaf'
+
+    return eaf_path
