da3f2740bc2a35a860ce75cd56a7153ae7ea3a50
Zhenya <e.kalenkovich@gmail.com>
2022-09-27 10:31:00 -0400

    feat(VIHI): do not try to parse invalid code_num values in eafs
    The only case that was and now isn't handled are string like "10." which can't be directly cast to int.
    We want to correct the values in eafs in such cases, not work around them.
    
    Other case that wasn't and isn't handled is empty strings.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="blabpy",
-    version="0.11.3",
+    version="0.11.4",
     packages=find_packages(),
     python_requires='>=3.7',
     install_requires=['pandas', 'numpy', 'pyarrow', 'pympi-ling', 'pydub', 'StrEnum'],
