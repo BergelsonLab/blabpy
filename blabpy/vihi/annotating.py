d01b94c2d611379ef5011eab233911ac6f4670f7
Zhenya Kalenkovich <e.kalenkovich@gmail.com>
2023-04-11 16:45:04 -0400

    checkout individual annotation folder

---
+++ blabpy/vihi/annotating.py
@@ -0,0 +1,28 @@
+from ..git_utils import sparse_clone
+from .paths import get_lena_recording_path, parse_full_recording_id, get_lena_path
+
+
+def checkout_recording_for_annotation(full_recording_id, annotator_name):
+    """
+    Checks out a recording from the LENA repo into an individual folder for the annotator.
+    :param full_recording_id: XX_MMM_NNN
+    :param annotator_name: "First Last"
+    :return: Path object to the checked-out folder.
+    """
+    pn_opus_repo_path = get_lena_path()
+    recording_folder = get_lena_recording_path(**parse_full_recording_id(full_recording_id))
+
+    # The folder name and the branch name contain both the recording ID and the annotator's name.
+    annotation_id = f'{full_recording_id}_{annotator_name.replace(" ", "-")}'
+
+    individual_folder = pn_opus_repo_path / 'annotations-in-progress' / annotation_id
+    _ = sparse_clone(
+        remote_uri=pn_opus_repo_path,
+        folder_to_clone_into=individual_folder,
+        checked_out_folder=recording_folder.relative_to(pn_opus_repo_path),
+        new_branch_name=None,
+        remote_name='GitHub',
+        source_branch='main',
+        depth=1)
+
+    return individual_folder
