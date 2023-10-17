f838b02a983cea9cd066cb40c424e128a073700d
Zhenya Kalenkovich <e.kalenkovich@gmail.com>
2023-10-17 10:15:54 -0400

    clone to a temp folder first
    This avoids a situation where the clone fails and we end up with an empty folder in "annotations-in-progress".
    
    Extra: more fixes to the paths to reflect the VIHI/LENA/ folder re-haul from around October 2023.

--- blabpy/vihi/annotating.py
+++ blabpy/vihi/annotating.py
@@ -1,3 +1,6 @@
+import shutil
+import tempfile
+
 from ..git_utils import sparse_clone
 from .paths import get_lena_recording_path, parse_full_recording_id, get_lena_annotations_path, get_lena_path, \
     get_lena_annotations_in_progress_path
@@ -23,20 +26,23 @@ def checkout_recording_for_annotation(full_recording_id, annotator_name):
     if individual_folder.exists():
         raise Exception(f'There is already a folder with in-progress annotations at\n'
                         f'{individual_folder.as_posix()}\n'
-                        f'Continue annotating in that folder.\n'
-                        f'\n'
-                        f'If, however, that folder is empty or doesn\'t contain the annotation files,\n'
-                        f'it is possible that an error occurred while the script created that folder.\n'
-                        f'In that case, delete the folder completely and re-run the command.')
+                        f'Continue annotating in that folder.')
 
-    _ = sparse_clone(
-        remote_uri=pn_opus_repo_path,
-        folder_to_clone_into=individual_folder,
-        checked_out_folder=recording_folder_in_repo,
-        new_branch_name=new_branch_name,
-        remote_name='vihi_main',
-        source_branch='main',
-        mark_folder_as_safe=True,
-        depth=1)
+    temp_dir_root = get_lena_path() / '.tmp'
+    temp_dir_root.mkdir(exist_ok=True)
+    with tempfile.TemporaryDirectory(dir=temp_dir_root) as temp_dir:
+        _ = sparse_clone(
+            remote_uri=pn_opus_repo_path,
+            folder_to_clone_into=temp_dir,
+            checked_out_folder=recording_folder_in_repo,
+            new_branch_name=new_branch_name,
+            remote_name='vihi_main',
+            source_branch='main',
+            mark_folder_as_safe=True,
+            depth=1)
+
+        # Only if the cloning was successful, move the temporary directory to the target location.
+        print('Clone finished. Moving files to "annotations-in-progress".')
+        shutil.move(temp_dir, individual_folder)
 
     return individual_folder
